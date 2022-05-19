f9815561183068c7662779602d857c17dd40e2a1
João Inácio <egnawake@gmail.com>
2022-05-19 23:36:37 +0100

    Run game in Main

---
+++ TicTacToeMVC/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TicTacToeMVC
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Game game = new Game();
+
+            IView consoleView = new ConsoleView();
+
+            Controller controller = new Controller(game, consoleView);
+            controller.Run();
+        }
+    }
+}
